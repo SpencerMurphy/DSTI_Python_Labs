91962d2660cd87cc9a54545d2c447826c397b7c5
Eric Kiennemann <eric.kiennemann@edu.dsti.institute>
2020-10-26 11:31:54 +0100

    add comment on server.py and camera.py

--- flask/camera.py
+++ flask/camera.py
@@ -6,57 +6,133 @@ from keras.preprocessing import image
 from keras.applications.vgg16 import preprocess_input as preprocess_input_vgg
 from keras.models import load_model
 
-model_front = load_model("model/weights_thumb_v1.h5")
-model_front.optimizer.lr
+# load the weights of the thumb detection model
+model_thumb = load_model("model/weights_thumb_v1.h5")
+model_thumb.optimizer.lr
 
+# initialiaze a global variable for thread
 thread = None
 
 
 class Camera:
+	"""
+    This Class define the bahavior of a camera connected to the server
+	"""
 	def __init__(self,video_source=0):
+		"""
+		This function initialize the class Camera
+		Args:
+		-----
+		- video_source : the id of the camera. This should enable to connect several cameras at the same time to the server
+		Returns:
+		--------
+		-  None
+		"""
 		self.video_source = video_source
+		# get the videoCapture instance from the cv2 library
 		self.camera = cv2.VideoCapture(self.video_source)
 		self.frames = []
+		# flag to know if the thread responsible for getting the frames is running
 		self.isrunning = False
 	def __del__(self):
+		"""
+		This function delete the class Camera
+		Args:
+		-----
+		- None
+		Returns:
+		--------
+		- None
+		"""		
 		self.camera.release(self.camera.video_source)
 	def run(self):
+		"""
+		This function start a thread in which the frame acquisition will be done
+		Args:
+		-----
+		- None
+		Returns:
+		--------
+		- None
+		"""
 		global thread
+		# only create a thread the first time this function is called
 		if thread is None:
+			# build and start a thread running _capture_loop
 			thread = threading.Thread(target=self._capture_loop,daemon=True)
 			self.isrunning = True
 			thread.start()
 
 	def _capture_loop(self):
+		"""
+		This function update the property frames with the image read from the camera flow
+		Args:
+		-----
+		- None
+		Returns:
+		--------
+		- None
+		"""
+		# as long as the thread is running, we get images from the camera
 		while self.isrunning:
 			v,im = self.camera.read()
+			# if the read works well then we store the image in the frames property
 			if v:
 				self.frames = im
+		# when stopping the thread, we stop the camera
 		self.camera.release()
 		
 
 	def stop(self):
+		"""
+		This function stop the thread
+		Args:
+		-----
+		- None
+		Returns:
+		--------
+		- None
+		"""
 		self.isrunning = False
 
 
 	def get_frame(self):
-		global model_front
+		"""
+		This function realize the prediction and show the result of this prediction on the frame
+		Args:
+		-----
+		- None
+		Returns:
+		--------
+		- None
+		"""
+		# use the model_variable as global
+		global model_thumb
+		# if an image has already been set into frames variable
 		if len(self.frames)>0:
 			img2 = self.frames
+			# flip the image to better feeling on the screen
 			img2 = cv2.flip(img2, 1)
+			# add a red rectangle on the image (where the thumb will be detected)
 			cv2.rectangle(img2, (300, 50), (550, 300), (0, 0, 255), 0)
+			# for the prediction we just keep this rectangle
 			img_array = cv2.resize(img2[50:300,300:550], (224, 224))
+			# pre traitment of the image before prediction
 			img_expanded = np.expand_dims(img_array, axis=0)
 			preprocessed_image = preprocess_input_vgg(img_expanded)
-			pred = model_front.predict(preprocessed_image)
+			# the prediction is done here and put into pred variable
+			pred = model_thumb.predict(preprocessed_image)
 			#print(round(pred[0][0],2),round(pred[0][1],2),round(pred[0][2],2))
 
 			classes = ['UP','DOWN','UNKNOWN']
+			# look at the higher probability among up/down/unknown
 			pred_class_idx = np.argmax(pred, axis=1)
 			text = "{}".format(classes[pred_class_idx[0]])
-
+			# add the prediction using a text, on the image
 			cv2.putText(img2, text, (35, 50), cv2.FONT_HERSHEY_SIMPLEX,1.25, (0, 0, 255), 5)
+			#encode the image in a .png file
 			img = cv2.imencode('.png', img2)[1].tobytes()
+		# if not image yet from the camera then just send a default image
 		else:
 			with open("images/0.jpg","rb") as f:
 				img = f.read()			
--- flask/server.py
+++ flask/server.py
@@ -10,26 +10,27 @@ camera = Camera()
 app = Flask(__name__)
 
 def gen(camera):
-	 """
+    """
     Function defined to create a generator : this generator get a frame( image) 
 	from the camera and provide a specific structure (in the yield command) that can be send to
 	the web page 
-    Args:
-    -----
-    - camera : instance of the class Camera
-    Returns:
-    --------
-    - specific structure return by the yield function including the frame
+	Args:
+	-----
+	- camera : instance of the class Camera
+	Returns:
+	--------
+	- specific structure return by the yield function including the frame
+
     """
-	while True:
-		frame = camera.get_frame()
-		yield (b'--frame\r\n'
-			   b'Content-Type: image/png\r\n\r\n' + frame + b'\r\n')
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/png\r\n\r\n' + frame + b'\r\n')
 
 
 @app.route('/' , methods=["POST", "GET"])
 def index():
-	"""
+    """
     This function treat the behavior of the index.html page
     Args:
     -----
@@ -37,16 +38,16 @@ def index():
     Returns:
     --------
     -  GET : return the template index.html when the web client connect to it
-	-  POST : redicrect to the route /fruit_result (temporary code)specific structure return by the yield function including the frame
+    -  POST : redicrect to the route /fruit_result (temporary code)specific structure return by the yield function including the frame
     """
-	if request.method == "POST":
-		return redirect(url_for("fruit_result"))
-	else:
-		return render_template("index.html")
+    if request.method == "POST":
+        return redirect(url_for("fruit_result"))
+    else:
+        return render_template("index.html")
 
 @app.route('/fruit_result')
 def fruit_result():
-	"""
+    """
     This function treat the behavior of the fruit_result page
     Args:
     -----
@@ -54,26 +55,26 @@ def fruit_result():
     Returns:
     --------
     -  GET : return the template fruit_result.html when the client connect to it. Simulate the fact that a banana has benn predicted
-	         and send the text "banana" to be displayed on the web page
-	-  		 After a timer time redirect to the route /thumb_video
+             and send the text "banana" to be displayed on the web page
+    -  		 After a timer time redirect to the route /thumb_video
     """
-	# use a global Timer_wait variable. Since a refresh of the web page has been set 
-	# this function is called every 5 seconds. So after 5 seconds the function redirect to
-	# the route /fruit_result.html
-	# THIS IS A TEMPORARY SOLUTION TO GO TO NEXT PAGE WITHOUT INTERACTION OF THE USER
-	global Timer_wait
-	Timer_wait += 1
-	# 
-	if Timer_wait < 2:
-		# here the fixe value of fruit is TEMPORARY : should be the result of a prediction
-		# the "fruit" value is dynamically rendered in the fruit_result.html page
-		return render_template('fruit_result.html',fruit = "banana")
-	else:
-		return redirect(url_for("thumb_video"))
+    # use a global Timer_wait variable. Since a refresh of the web page has been set 
+    # this function is called every 5 seconds. So after 5 seconds the function redirect to
+    # the route /fruit_result.html
+    # THIS IS A TEMPORARY SOLUTION TO GO TO NEXT PAGE WITHOUT INTERACTION OF THE USER
+    global Timer_wait
+    Timer_wait += 1
+    # 
+    if Timer_wait < 2:
+        # here the fixe value of fruit is TEMPORARY : should be the result of a prediction
+        # the "fruit" value is dynamically rendered in the fruit_result.html page
+        return render_template('fruit_result.html',fruit = "banana")
+    else:
+        return redirect(url_for("thumb_video"))
 
 @app.route('/thumb_video', methods=["POST", "GET"])
 def thumb_video():
-	"""
+    """
     This function treat the behavior of the thumb_video page
     Args:
     -----
@@ -81,38 +82,38 @@ def thumb_video():
     Returns:
     --------
     -  GET : return the template thumb_video.html . Simulate the fact that a banana has benn predicted
-	         and send the text "banana" to be displayed on the web page
-	-  POST : redicrect to the route /thumb_result : TEMPORARY CODE this POST methods is activated by a button pressed
-			  on the page : the target page should be defined depending on the result of the thumb prediction
+             and send the text "banana" to be displayed on the web page
+    -  POST : redicrect to the route /thumb_result : TEMPORARY CODE this POST methods is activated by a button pressed
+              on the page : the target page should be defined depending on the result of the thumb prediction
     """
-	if request.method == "POST":
-		# the camera is stopped because the prediction is finished
-		camera.stop()
-		return redirect(url_for("thumb_result"))
-	else :
-		# when the page is rendered, the camera is started to realize the prediction
-		# the code <img src="{{ url_for('thumb_video_feed') }}"> in the thumb_video.html file
-		# "call" the route /thumb_video_feed in order to get an image
-		camera.run()
-		return render_template('thumb_video.html')
+    if request.method == "POST":
+        # the camera is stopped because the prediction is finished
+        camera.stop()
+        return redirect(url_for("thumb_result"))
+    else :
+        # when the page is rendered, the camera is started to realize the prediction
+        # the code <img src="{{ url_for('thumb_video_feed') }}"> in the thumb_video.html file
+        # "call" the route /thumb_video_feed in order to get an image
+        camera.run()
+        return render_template('thumb_video.html')
 
 @app.route("/thumb_video_feed")
 def thumb_video_feed():
-	"""
+    """
     This function is "called" by the webpage "thumb_video_feed". and it deliver images to this web page using an understandable
-	structure
+    structure
     Args:
     -----
     - None
     Returns:
     --------
     -  a "Response" that can be displayed as an image by the web page. Using the generator to get images from the camera
-	"""
-	return Response(gen(camera), mimetype="multipart/x-mixed-replace; boundary=frame")
+    """
+    return Response(gen(camera), mimetype="multipart/x-mixed-replace; boundary=frame")
 
 @app.route('/thumb_result/')
 def thumb_result():
-	"""
+    """
     This function treat the behavior of the thumb_result page
     Args:
     -----
@@ -120,13 +121,13 @@ def thumb_result():
     Returns:
     --------
     -  GET : return the template thumb_result.html .
-	"""
-	return render_template('thumb_result.html')
+    """
+    return render_template('thumb_result.html')
 
 # Fruit_video not yet coded
 @app.route('/fruit_video/')
 def fruit_video():
-	"""
+    """
     This function treat the behavior of the fruit_video page
     Args:
     -----
@@ -134,13 +135,13 @@ def fruit_video():
     Returns:
     --------
     -  GET : return the template fruit_video.html .
-	"""
-	return render_template('fruit_video.html')
+    """
+    return render_template('fruit_video.html')
 
 # fruit_manual_choice not yet coded
 @app.route('/fruit_manual_choice/')
 def fruit_manual_choice():
-	"""
+    """
     This function treat the behavior of the fruit_manual_choice page
     Args:
     -----
@@ -148,13 +149,13 @@ def fruit_manual_choice():
     Returns:
     --------
     -  GET : return the template fruit_manual_choice.html .
-	"""
-	return render_template('fruit_manual_choice.html')
+    """
+    return render_template('fruit_manual_choice.html')
 
 # ticket_print not yet coded
 @app.route('/ticket_print/')
 def ticket_print():
-	"""
+    """
     This function treat the behavior of the ticket_print page
     Args:
     -----
@@ -162,9 +163,9 @@ def ticket_print():
     Returns:
     --------
     -  GET : return the template ticket_print.html .
-	"""
-	return render_template('ticket_print.html')
+    """
+    return render_template('ticket_print.html')
    
 if __name__ == "__main__":
-	Timer_wait = 0
-	app.run(debug=True)
+    Timer_wait = 0
+    app.run(debug=True)
